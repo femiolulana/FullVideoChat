8f187001bbda9a899cccf9799c4ff740439941b9
femiolulana <oniseikeji@gmail.com>
2024-01-17 05:07:20 -0500

    set up agora

--- mychat/static/js/streams.js
+++ mychat/static/js/streams.js
@@ -1,4 +1,21 @@
 const APP_ID = "65ca955fe4304ff48382163b1aa10a17"
 const CHANNEL = 'main'
 const TOKEN = '007eJxTYEj6lmmWw+auZ2kXkX3NiC/ZquSjxNSAbzJXtoS8+frrWKwCg5lpcqKlqWlaqomxgUlamomFsYWRoZlxkmFioqFBoqF56OzlqQ2BjAyFm44wMTJAIIjPwpCbmJnHwAAA9Moe5g=='
+let UID;
 
+
+const client = AgoraRTC.createClient({mode:'rtc', codec:'vp8'})
+
+let localTracks = []
+let remoteUsers = {}
+
+let joinAndDisplayLocalStream = async () => {
+    UID = await client.join(APP_ID, CHANNEL, TOKEN, null)
+
+    localTracks = await AgoraRTC.createMicrophoneAndCameraTracks()
+
+    let player = `<div class="video-container" id="user-container-${UID}">
+            <div id="username-wrapper"><span class="user-name">My name</span></div>
+            <div class="video-player" id="user-${UID}"></div>
+        </div>`
+}
