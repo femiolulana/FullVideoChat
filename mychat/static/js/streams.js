e08f650a3e8a7e69b38d9b531f339f96b6d75392
femiolulana <oniseikeji@gmail.com>
2024-01-17 04:47:15 -0500

    add agora sdk

---
+++ mychat/static/js/streams.js
@@ -0,0 +1,4 @@
+const APP_ID = "65ca955fe4304ff48382163b1aa10a17"
+const CHANNEL = 'main'
+const TOKEN = '007eJxTYEj6lmmWw+auZ2kXkX3NiC/ZquSjxNSAbzJXtoS8+frrWKwCg5lpcqKlqWlaqomxgUlamomFsYWRoZlxkmFioqFBoqF56OzlqQ2BjAyFm44wMTJAIIjPwpCbmJnHwAAA9Moe5g=='
+
